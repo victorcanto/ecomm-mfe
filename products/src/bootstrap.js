077701183e428643acd87051864c6dbd8c67d0d8
Victor Canto <iamvictorcanto@gmail.com>
2022-03-26 03:29:07 -0300

    feat: App context/situation

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -9,3 +9,14 @@ for (let i = 0; i < PRODUCTS_QTD; i++) {
 }
 
 document.querySelector('#dev-products').innerHTML = products;
+
+// Context/Situation #1
+// Nós vamos executar este arquivo em desenvolvimento isoladamente.
+// Mós estamos usando nosso arquivo index.html localmente.
+// Definitivamente, temos um elemento com um id of 'dev-products'
+// Queremos renderizar nosso app nesse elemento, sem atraso, pois estamos em desenvolvimento.
+
+// Context/Situation #1
+// Nós vamos executar este arquivo em desenvolvimento ou produção através do CONTAINER app.
+// NÃO HÁ GARANTIA que existe um elemento com o id 'dev-products'
+// Portanto, não queremos renderizer imediatamente o app, porque esse elemento que estamos procurando pode não existir.
