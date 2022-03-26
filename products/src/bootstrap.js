97ec6414e8b1bbf56e325484a8751f9e0a5a8e8b
Victor Canto <iamvictorcanto@gmail.com>
2022-03-26 02:47:55 -0300

    feat: shares deps with mfes outside the container

---
+++ products/src/bootstrap.js
@@ -0,0 +1,11 @@
+import faker from 'faker';
+
+let products = '';
+const PRODUCTS_QTD = 5;
+
+for (let i = 0; i < PRODUCTS_QTD; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+
+document.querySelector('#dev-products').innerHTML = products;
