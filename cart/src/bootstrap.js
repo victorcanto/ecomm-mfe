d2da20f88c725ffb8f81d281290c39109429751f
Victor Canto <iamvictorcanto@gmail.com>
2022-03-26 04:04:18 -0300

    refactor: cart with mount function

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,16 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (htmlEl) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+  htmlEl.innerHTML = cartText;
+};
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
