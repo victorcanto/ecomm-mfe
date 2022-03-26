b57c2aaad81e0dca67370ea104140c864b52e86f
Victor Canto <iamvictorcanto@gmail.com>
2022-03-26 04:04:43 -0300

    feat: use mount functions in container-mfe

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow'
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log('Container!');
+productsMount(document.querySelector('#app-products'));
+cartMount(document.querySelector('#app-cart'));
