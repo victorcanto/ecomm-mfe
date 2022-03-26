3019393d6a6524e53a322624f34dfad12402e326
Victor Canto <iamvictorcanto@gmail.com>
2022-03-26 01:18:27 -0300

    feat(container):  set MF to fetch those files

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,12 @@ module.exports = {
   },
 
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        products: 'products@http://localhost:8081/remoteEntry.js',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
